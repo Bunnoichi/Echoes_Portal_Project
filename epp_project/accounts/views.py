8f608b91ad1735f09754ba7333aca5a8bf7fbed7
Bunnoichi <furuto.3866.miyago@gmail.com>
2025-10-29 20:37:33 +0900

    ログイン必須ビューで・ログアウト状態だったときの画面遷移を実装。

--- epp_project/accounts/views.py
+++ epp_project/accounts/views.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView, LogoutView
 from .forms import SignUpForm, LoginForm
 from .models import CustomUser
+from django.contrib import messages
 
 class SignUpView(CreateView):
     model = CustomUser
@@ -14,6 +15,12 @@ class SignUpView(CreateView):
 class CustomLoginView(LoginView):
     form_class = LoginForm
     template_name = 'accounts/login.html'
+
+    def get(self, request, *args, **kwargs):
+        # ?next= がURLについている場合、ログイン必須ページから来たとみなす
+        if 'next' in request.GET:
+            messages.info(request, 'このページを表示するにはログインが必要です。')
+        return super().get(request, *args, **kwargs)
 
 
 class CustomLogoutView(LogoutView):
