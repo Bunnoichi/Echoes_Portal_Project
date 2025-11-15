e3fc394b8a526b88f5cf5826d0e86e299d5fa66b
Bunnoichi <furuto.3866.miyago@gmail.com>
2025-11-15 17:07:56 +0900

    初期修正

--- epp_project/website_app/forms.py
+++ epp_project/website_app/forms.py
@@ -4,7 +4,7 @@ from .models import Team, Report
 class TeamForm(ModelForm):
    class Meta:
       model = Team
-      fields = ['team_name', 'team_desc', 'duration_reha', 'duration_onst', 'onstage_time', 'onstage_time_acc',  'checkin_pretime_1', 'checkin_postime_1','checkin_pretime_2', 'checkin_postime_2', 'file_form', 'file_stage']
+      fields = ['team_name', 'team_name_kana', 'team_desc', 'team_ninzu', 'team_rep', 'team_rep_mail', 'team_rep_tel', 'checkin_pretime_1', 'checkin_postime_1', 'checkin_pretime_2', 'checkin_postime_2', 'duration_reha', 'duration_onst', 'onstage_time', 'onstage_time_acc', 'file_form', 'file_stage', 'team_equip', 'note']
 
    def __init__(self, *args, **kwargs):
       view_type = kwargs.pop('view_type', None)
@@ -15,7 +15,7 @@ class TeamForm(ModelForm):
             pass
 
          case 'initial':
-            remove_fields = ['onstage_time_acc', 'checkin_postime_1', 'checkin_charge_1', 'checkin_postime_2', 'checkin_charge_2']
+            remove_fields = ['onstage_time_acc', 'checkin_postime_1', 'checkin_postime_2']
             for name in remove_fields:
                self.fields.pop(name, None)
          
--- epp_project/website_app/models.py
+++ epp_project/website_app/models.py
@@ -5,19 +5,24 @@ import uuid
 class Team(models.Model):
    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name="ID")
    team_name = models.CharField(max_length=100, verbose_name="団体名")
-   team_desc = models.CharField(max_length=100, verbose_name="団体の説明")
+   team_name_kana = models.CharField(verbose_name='団体名（カナ）')
+   team_desc = models.CharField(max_length=100, verbose_name="団体の所属", null=True, blank=True)
+   team_ninzu = models.IntegerField(verbose_name='参加人数', null=True, blank=True)
+   team_equip = models.TextField(verbose_name='使用機材等',max_length=1000, null=True, blank=True)
+   team_rep = models.CharField(verbose_name="代表者名（カナ）",null=True, blank=True)
+   team_rep_mail = models.CharField(verbose_name="代表者メールアドレス",null=True, blank=True)
+   team_rep_tel = models.CharField(verbose_name="代表者電話番号",null=True, blank=True)
    checkin_pretime_1 = models.DateTimeField(verbose_name="受付日時の予定")
    checkin_postime_1 = models.DateTimeField(verbose_name="実際の受付日時",null=True, blank=True)
    checkin_pretime_2 = models.DateTimeField(verbose_name="直前待機日時の予定", null=True, blank=True)
    checkin_postime_2 = models.DateTimeField(verbose_name="実際の直前待機日時",null=True, blank=True)
-   checkin_charge_1 = models.CharField(verbose_name="担当者",null=True, blank=True)
-   checkin_charge_2 = models.CharField(verbose_name="担当者",null=True, blank=True)
    duration_reha = models.IntegerField(verbose_name='リハ分数', default=5, null=True, blank=True)
    duration_onst = models.IntegerField(verbose_name='出演分数', default=10, null=True, blank=True)
    onstage_time = models.DateTimeField(verbose_name="出演予定時刻")
    onstage_time_acc = models.DateTimeField(verbose_name="実際の出演時刻", null = True, blank=True)
    file_form = models.FileField(upload_to='website_app/form_files/',null=True,  blank=True, verbose_name='応募ファイル')
    file_stage = models.FileField(upload_to='website_app/stage_files/',null=True, blank=True, verbose_name='ステージファイル')
+   note = models.TextField(verbose_name='備考',max_length=1000, null=True, blank=True)
    created_at = models.DateTimeField(auto_now_add=True, verbose_name="作成日時")
    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新日時")
 
@@ -25,12 +30,12 @@ class Team(models.Model):
       return self.team_name
    
    class Meta:
-    permissions = [
-        ("initial_registration", "団体初期登録権限"),
-        ("checkin", "打刻権限"),
-        ("view_detail", "団体詳細確認権限"),
-        ("update_information", "団体情報更新権限"),
-    ]
+      permissions = [
+         ("initial_registration", "団体初期登録権限"),
+         ("checkin", "打刻権限"),
+         ("view_detail", "団体詳細確認権限"),
+         ("update_information", "団体情報更新権限"),
+      ]
    
 choices_place = {
    '受付': '受付',
