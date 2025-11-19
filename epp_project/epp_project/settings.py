875fb7a7b74e90b1036f706504d7fb5fb195ffff
Bunnoichi <furuto.3866.miyago@gmail.com>
2025-11-19 16:18:29 +0900

    軽微な修正

--- epp_project/epp_project/settings.py
+++ epp_project/epp_project/settings.py
@@ -109,7 +109,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
 TIME_ZONE = 'Asia/Tokyo'
 
